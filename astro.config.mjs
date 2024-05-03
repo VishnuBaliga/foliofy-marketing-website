4efaa4130e0925d2ff65ac895fce68bb67ec37e1
Marshall Shelly <Marshallshelly97@gmail.com>
2024-05-03 21:34:13 +0530

    added scripts bring contents of outdir to root

--- astro.config.mjs
+++ astro.config.mjs
@@ -12,4 +12,5 @@ export default defineConfig({
     format: "file",
   },
   outDir: "landing-page",
+  compressHTML: true,
 });
---
+++ setup.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+mv /landing-page/landing-page /landing-page
